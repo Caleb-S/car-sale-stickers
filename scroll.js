8f894714e90f658348bde134b260d29a9e7f35e4
Caleb-S <sass.caleb@icloud.com>
2023-07-28 01:09:00 +1000

    working version of soft scroll function

--- scroll.js
+++ scroll.js
@@ -1,49 +1,40 @@
 document.addEventListener("DOMContentLoaded", function () {
     const scrollbox = document.querySelector(".product-scrollbox");
     const scrollSpeed = 1; // Adjust this value to control the scrolling speed
-    const scrollThreshold = 0.30; // 25% away from the top or bottom
+    const scrollThreshold = 0.15; // 25% away from the top or bottom
 
-
+    let isScrolling = false;
     let scrollDirection = 0;
 
-    // Function to scroll the container
     function scrollBox() {
         if (isScrolling && scrollDirection !== 0) {
-            // Apply smooth scrolling
             scrollbox.scrollTop += scrollDirection * scrollSpeed;
         }
         requestAnimationFrame(scrollBox);
     }
 
-    // Start the scrolling loop
     requestAnimationFrame(scrollBox);
 
     scrollbox.addEventListener("mousemove", function (event) {
         const scrollboxHeight = scrollbox.clientHeight;
-        const scrollableHeight = scrollbox.scrollHeight - scrollboxHeight;
-        const mouseY = event.clientY;
+        const mouseY = event.clientY - scrollbox.getBoundingClientRect().top;
 
-        // Check distance from top and bottom
         const distanceFromTop = scrollbox.scrollTop;
-        const distanceFromBottom = scrollableHeight - scrollbox.scrollTop;
+        const distanceFromBottom = scrollbox.scrollHeight - scrollbox.scrollTop - scrollboxHeight;
         const distanceFromThreshold = scrollboxHeight * scrollThreshold;
 
-        // Determine the scroll direction
-        if (distanceFromTop <= distanceFromThreshold && mouseY < distanceFromThreshold) {
-            scrollDirection = -1; // Scroll up
+        if (mouseY < distanceFromThreshold) {
+            // Mouse near the top edge, scroll up
+            scrollDirection = -1;
             isScrolling = true;
-        } else if (distanceFromBottom <= distanceFromThreshold && mouseY > scrollboxHeight - distanceFromThreshold) {
-            scrollDirection = 1; // Scroll down
+        } else if (mouseY > scrollboxHeight - distanceFromThreshold) {
+            // Mouse near the bottom edge, scroll down
+            scrollDirection = 1;
             isScrolling = true;
         } else {
-            scrollDirection = 0; // Not scrolling
+            // Not scrolling
+            scrollDirection = 0;
             isScrolling = false;
         }
-    });
-
-    scrollbox.addEventListener("mouseleave", function () {
-        // Stop scrolling when the mouse leaves the scrollbox
-        isScrolling = false;
-        scrollDirection = 0;
     });
 });
