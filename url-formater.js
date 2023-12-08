7c0fe6e215365a13f0f035b2ca7a99c3d582d9db
Caleb-S <sass.caleb@icloud.com>
2023-12-08 19:15:25 +1000

    Format url

---
+++ url-formater.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // Get the current URL
+
+
+    formatUrl();
+
+    // Introduce a one-second delay before updating the browser's URL
+    setTimeout(() => {
+        formatUrl();
+        // Update the browser's URL without reloading the page
+        //  window.history.replaceState({}, document.title, cleanUrl);
+    }, 1); // 1000 milliseconds = 1 second
+});
+
+
+function formatUrl() {
+    var currentUrl = window.location.href;
+
+    // Check if the URL ends with "index.html"
+    if (currentUrl.endsWith("/index.html")) {
+        // Remove only the "index.html" part
+        var cleanUrl = currentUrl.replace(/\/index\.html$/, '/');
+    } else {
+        // Remove the ".html" part for other URLs
+        var cleanUrl = currentUrl.replace(/\.html$/, '');
+    }
+    window.history.replaceState({}, document.title, cleanUrl);
+}
