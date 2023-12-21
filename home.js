bb837a83b75ace68049f2748a37df038fc3ff123
Caleb-S <sass.caleb@icloud.com>
2023-12-21 14:59:13 +1000

    safari autoplay

--- home.js
+++ home.js
@@ -581,12 +581,12 @@ document.addEventListener("DOMContentLoaded", () => {
                 //const elapsed = (currentTime - initialTimestamp) / 1000; // Subtract 5 seconds to start earlier
 
                 var elapsed = timeCounter;
-
-                if ('mediaSession' in navigator && 'setPositionState' in navigator.mediaSession) {
-                    // Set mediaPlaybackRequiresUserAction to false
-                    video.mediaPlaybackRequiresUserAction = false;
-                }
-
+                /*
+                                if ('mediaSession' in navigator && 'setPositionState' in navigator.mediaSession) {
+                                    // Set mediaPlaybackRequiresUserAction to false
+                                    video.mediaPlaybackRequiresUserAction = false;
+                                }
+                */
                 video.load();
 
                 video.addEventListener("loadeddata", function () {
@@ -603,6 +603,8 @@ document.addEventListener("DOMContentLoaded", () => {
                     }).catch(error => {
                         // Autoplay was prevented, handle the error
                         console.error("Autoplay not allowed:", error);
+                        video.mediaPlaybackRequiresUserAction = false;
+                        video.play();
                     });
                 }
                 isVideoPlaying = true;
