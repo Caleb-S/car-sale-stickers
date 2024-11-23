4208adcf7fae7f59aeb959bece6928f34f7c09b1
Caleb-S <sass.caleb@icloud.com>
2024-11-23 12:43:08 +1000

    scroll working

--- src/scripts/scroll.js
+++ src/scripts/scroll.js
@@ -1,14 +1,54 @@
 document.addEventListener("DOMContentLoaded", function () {
+    const scrollSpeed = 1;
+    const scrollThreshold = 0.10;
 
     let isScrolling = false;
     let scrollDirection = 0;
+    let scrollbox = document.querySelector(".product-scrollbox");
 
-    
+    function scrollBox() {
+        if (isScrolling && scrollDirection !== 0) {
+            scrollbox.scrollTop += scrollDirection * scrollSpeed;
+        }
+        requestAnimationFrame(scrollBox);
+    }
 
     requestAnimationFrame(scrollBox);
 
-    let scrollbox = document.querySelector(".product-scrollbox");
-    scrollbox.addEventListener("mousemove", onMouseMove.bind(null, scrollbox, isScrolling, scrollDirection));
+    scrollbox.addEventListener("mousemove", function (event) {
+        let scrollboxHeight = scrollbox.clientHeight;
+        let mouseY = event.clientY - scrollbox.getBoundingClientRect().top;
+
+        let distanceFromTop = scrollbox.scrollTop;
+        let distanceFromBottom = scrollbox.scrollHeight - scrollbox.scrollTop - scrollboxHeight;
+        let distanceFromThreshold = scrollboxHeight * scrollThreshold;
+
+        let screenWidth = document.documentElement.clientWidth;
+
+        if (!(screenWidth >= 0 && screenWidth <= 639)) {
+            if (scrollDirection === 1 && mouseY < distanceFromThreshold) {
+
+                scrollDirection = 1;
+                isScrolling = true;
+            } else if (scrollDirection === -1 && mouseY > scrollboxHeight - distanceFromThreshold) {
+
+                scrollDirection = -1;
+                isScrolling = true;
+            } else if (mouseY < distanceFromThreshold) {
+
+                scrollDirection = -1;
+                isScrolling = true;
+            } else if (mouseY > scrollboxHeight - distanceFromThreshold) {
+
+                scrollDirection = 1;
+                isScrolling = true;
+            } else if (scrollDirection != 0) {
+
+                scrollDirection = 0;
+                isScrolling = false;
+            }
+        }
+    });
 
     function stopScrolling() {
         isScrolling = false;
@@ -17,53 +57,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
     scrollbox.addEventListener("mouseleave", stopScrolling);
 });
-
-function onMouseMove(event, isScrolling, scrollDirection) {
-    const scrollSpeed = 1;
-    const scrollThreshold = 0.10;
-    let scrollboxHeight = scrollbox.clientHeight;
-    let mouseY = event.clientY - scrollbox.getBoundingClientRect().top;
-
-    let distanceFromTop = scrollbox.scrollTop;
-    let distanceFromBottom = scrollbox.scrollHeight - scrollbox.scrollTop - scrollboxHeight;
-    let distanceFromThreshold = scrollboxHeight * scrollThreshold;
-
-    let screenWidth = document.documentElement.clientWidth;
-
-    if (!(screenWidth >= 0 && screenWidth <= 639)) {
-        if (scrollDirection === 1 && mouseY < distanceFromThreshold) {
-
-            scrollDirection = 1;
-            isScrolling = true;
-        } else if (scrollDirection === -1 && mouseY > scrollboxHeight - distanceFromThreshold) {
-
-            scrollDirection = -1;
-            isScrolling = true;
-        } else if (mouseY < distanceFromThreshold) {
-
-            scrollDirection = -1;
-            isScrolling = true;
-        } else if (mouseY > scrollboxHeight - distanceFromThreshold) {
-
-            scrollDirection = 1;
-            isScrolling = true;
-        } else if (scrollDirection != 0) {
-
-            scrollDirection = 0;
-            isScrolling = false;
-        }
-    }
-}
-
-
-
-function scrollBox() {
-    let scrollbox = document.querySelector(".product-scrollbox");
-    if (isScrolling && scrollDirection !== 0) {
-        scrollbox.scrollTop += scrollDirection * scrollSpeed;
-    }
-    requestAnimationFrame(scrollBox);
-}
 
 document.addEventListener("DOMContentLoaded", function () {
     let orderSubtotalDiv = document.querySelector(".order-subtotal");
